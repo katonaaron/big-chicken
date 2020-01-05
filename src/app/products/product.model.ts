ab460be4ee6e3d1fd4ee1fb7e84bd36de6b14414
Katona Áron <katonaaron01@gmail.com>
2020-01-05 14:47:03 +0200

    product model updated

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -3,6 +3,9 @@ import {Url} from '../shared/url.model';
 export class Product {
   public 'imagePath': string;
   public 'name': string;
+  public 'ingredients': string;
+  public 'price': number;
+  public 'barcode': string;
   '_links': {
     'self': Url,
     'product': Url,
