87dd8bd490150c4a622232a842b4fd4258e9430f
Katona Áron <katonaaron01@gmail.com>
2020-01-05 12:57:34 +0200

    UserController renamed to TokenController

--- src/main/java/com/mastaron/bigchickenapi/controller/TokenController.java
+++ src/main/java/com/mastaron/bigchickenapi/controller/TokenController.java
@@ -1,53 +1,51 @@
 package com.mastaron.bigchickenapi.controller;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
 import com.mastaron.bigchickenapi.FirebaseUtil;
 import com.mastaron.bigchickenapi.model.User;
 import com.mastaron.bigchickenapi.repository.UserRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class TokenController {
     UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
+    public TokenController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/users/idToken/{idToken}")
-    public User getUserByIdToken(@PathVariable String idToken) {
-        String uid = null;
+    @GetMapping("/token")
+    public User getUserByIdToken(@RequestBody String FirebaseToken) {
+        String uid;
         try {
-            uid = FirebaseUtil.getUidFromIdToken(idToken);
+            uid = FirebaseUtil.getUidFromIdToken(FirebaseToken);
         } catch (FirebaseAuthException e) {
             throw new ResourceNotFoundException("Invalid token");
         }
         User user = userRepository.findByUid(uid);
-        if(user == null)
+        if (user == null)
             throw new ResourceNotFoundException("User was not found");
         return user;
     }
 
-    @PutMapping("/users/idToken/{idToken}")
-    public User createOrFetchUserFromByToken(@PathVariable String idToken){
-        String uid = null;
+    @PostMapping("/token")
+    public User createToken(@RequestBody String FirebaseToken) {
+        String uid;
         try {
-            uid = FirebaseUtil.getUidFromIdToken(idToken);
+            uid = FirebaseUtil.getUidFromIdToken(FirebaseToken);
         } catch (FirebaseAuthException e) {
             throw new ResourceNotFoundException("Invalid token");
         }
 
-        User user = null;
-        if(userRepository.existsByUid(uid)) {
+        User user;
+        if (userRepository.existsByUid(uid)) {
             user = userRepository.findByUid(uid);
-            if(user == null)
-                throw new ResourceNotFoundException("User was not found");
+            if (user == null)
+                throw new ResourceNotFoundException("User could not be retrieved");
         } else {
             user = new User();
             user.setUid(uid);
