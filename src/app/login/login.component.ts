cff1905868878f82b0edc709650dc5c24f4a7e1c
Katona Áron <katonaaron01@gmail.com>
2020-01-05 14:44:33 +0200

    added redirection after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  returnUrl: string;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(
+      (queryParams) => {
+        if (queryParams && queryParams.returnUrl) {
+          this.returnUrl = queryParams.returnUrl;
+        } else {
+          this.returnUrl = '/';
+        }
+      }
+    );
   }
 
+  successCallback() {
+    this.router.navigate([this.returnUrl]);
+  }
 }
