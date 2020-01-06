49ed81fbd3bf09523a05d1b7f7857d4ca8a90c30
Katona Áron <katonaaron01@gmail.com>
2020-01-06 17:20:06 +0200

    added selection and delete; css class renamed

--- src/app/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ShoppingCartItemProduct} from '../shopping-cart-item-product.model';
+import {ShoppingCartItemService} from '../shopping-cart-item.service';
 
 @Component({
   selector: 'app-shopping-cart-item',
@@ -10,10 +11,18 @@ export class ShoppingCartItemComponent implements OnInit {
   @Input() itemAndProduct: ShoppingCartItemProduct = null;
   quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor() {
+  constructor(private shoppingCartItemService: ShoppingCartItemService) {
   }
 
   ngOnInit() {
   }
 
+  onSelect(event: Event) {
+    this.itemAndProduct.shoppingCartItem.quantity = +(event.target as HTMLInputElement).value;
+    this.shoppingCartItemService.updateShoppingCartItem(this.itemAndProduct.shoppingCartItem).subscribe();
+  }
+
+  onDelete() {
+    this.shoppingCartItemService.deleteShoppingCartItem(this.itemAndProduct.shoppingCartItem).subscribe();
+  }
 }
