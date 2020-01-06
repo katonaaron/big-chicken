e773a22ebdf3018d5509f5f7b01d28bffedd306a
Katona Áron <katonaaron01@gmail.com>
2020-01-06 02:11:49 +0200

    shopping-cart-menu and category selection menu added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductCategory} from '../categories/product-category.model';
+import {ProductCategoryService} from '../categories/product-category.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = false;
+  productCategories: ProductCategory[] = [];
 
-  constructor() {
+  constructor(private productCategoryService: ProductCategoryService) {
   }
 
   ngOnInit() {
+    this.productCategoryService
+      .getAllProductCategories()
+      .subscribe(
+        (productCategoryCollection) => {
+          this.productCategories = productCategoryCollection._embedded.productCategories;
+        }
+      );
   }
 }
