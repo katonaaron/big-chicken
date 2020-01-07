6c21f7c40cec6b0256b9e2b462463e507efc67a6
Katona Áron <katonaaron01@gmail.com>
2020-01-07 03:31:13 +0200

    shopping cart services rewritten, results are stored in BehaviorSubject objects.

--- src/app/shopping-cart/shopping-cart-item.service.ts
+++ src/app/shopping-cart/shopping-cart-item.service.ts
@@ -1,10 +1,39 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../user.model';
+import {Product} from '../products/product.model';
+import {environment} from '../../environments/environment';
+import {ShoppingCartItemDTO} from './shopping-cart-item-dto.model';
+import {map} from 'rxjs/operators';
+import {ShoppingCartItem} from './shopping-cart-item.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartItemService {
+  private url = environment.ApiUrl + 'shoppingCartItems';
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  public createItem(user: User, product: Product): Observable<ShoppingCartItem> {
+    return this.http
+      .post<ShoppingCartItemDTO>(this.url, {
+        quantity: 1,
+        product: product._links.self.href,
+        user: user._links.self.href
+      })
+      .pipe(map(item => new ShoppingCartItem(item.quantity, item._links, product)));
+  }
+
+  public updateItem(shoppingCartItem: ShoppingCartItem): Observable<ShoppingCartItem> {
+    return this.http
+      .patch<ShoppingCartItemDTO>(shoppingCartItem._links.self.href, {
+        quantity: shoppingCartItem.quantity,
+        // product: shoppingCartItem.product._links.self.href,
+        // user: shoppingCartItem._links.user.href
+      })
+      .pipe(map(item => new ShoppingCartItem(item.quantity, item._links, shoppingCartItem.product)));
   }
 }
--- src/app/shopping-cart/shopping-cart.service.ts
+++ src/app/shopping-cart/shopping-cart.service.ts
@@ -1,95 +1,139 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {EMPTY, forkJoin, Observable, of} from 'rxjs';
-import {ShoppingCart} from './shopping-cart.model';
+import {BehaviorSubject, forkJoin, Observable, of} from 'rxjs';
 import {UserService} from '../user.service';
-import {flatMap, map} from 'rxjs/operators';
 import {ProductService} from '../products/product.service';
-import {ShoppingCartItemProduct} from './shopping-cart-item-product.model';
+import {ShoppingCartDTO} from './shopping-cart-dto.model';
+import {User} from '../user.model';
+import {map, mergeMap, switchMap, take, tap} from 'rxjs/operators';
+import {ShoppingCartItemDTO} from './shopping-cart-item-dto.model';
+import {Product} from '../products/product.model';
+import {ShoppingCartItem} from './shopping-cart-item.model';
+import {ShoppingCartItemService} from './shopping-cart-item.service';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
-  shoppingCart: Observable<ShoppingCart> = EMPTY;
-  itemsAndProducts: Observable<ShoppingCartItemProduct[]> = EMPTY;
-  numberOfProducts: Observable<number> = of(0);
-  total: Observable<number> = of(0);
+  items$ = new BehaviorSubject<ShoppingCartItem[]>([]);
+  total$ = new BehaviorSubject<number>(0);
+  count$ = new BehaviorSubject<number>(0);
 
   constructor(private http: HttpClient,
               private userService: UserService,
-              private productService: ProductService) {
-    this.shoppingCart = this.userService.user
-      .pipe(flatMap(
-        (user) => {
-          return this.getShoppingCartFromUrl(user._links.shoppingCart.href);
+              private productService: ProductService,
+              private shoppingCartItemService: ShoppingCartItemService,
+              private router: Router) {
+    this.getCartItems();
+    this.calculateTotal();
+    this.calculateCount();
+  }
+
+  public addProduct(product: Product): void {
+    this.userService.user.subscribe(user => {
+      if (!user) {
+        this.router.navigate(['/login'], {queryParams: {returnUrl: this.router.url}});
+      }
+    });
+    this.items$
+      .pipe(take<ShoppingCartItem[]>(1))
+      .pipe(switchMap(
+        (items) => {
+          const index = items.findIndex(item => item.product._links.self.href === product._links.self.href);
+          if (index !== -1) {
+            items[index].quantity++;
+            return this.shoppingCartItemService.updateItem(items[index]).pipe(map(() => items));
+          } else {
+            return this.userService.user
+              .pipe(mergeMap(
+                (user) => {
+                  if (user) {
+                    return this.shoppingCartItemService.createItem(user, product).pipe(
+                      map(item => [...items, item])
+                    );
+                  } else {
+                    return of<ShoppingCartItem[]>([]);
+                  }
+                }
+              ));
+          }
         }
-      ));
-    this.itemsAndProducts = this.getItemsAndProductsOfShoppingCart();
-    this.total = this.calculateTotal();
-    this.numberOfProducts = this.calculateNumberOfProducts();
+      ))
+      .subscribe(items => this.items$.next(items));
   }
 
-  public getShoppingCartFromUrl(url: string): Observable<ShoppingCart> {
-    return this.http.get<ShoppingCart>(url);
+  public deleteItem(item: ShoppingCartItem): void {
+    this.items$.pipe(
+      take(1),
+      switchMap(items => this.http.delete(item._links.self.href).pipe(
+        tap(() => items.splice(items.indexOf(item), 1)),
+        map(() => items)
+      ))
+    ).subscribe(items => this.items$.next(items));
   }
 
-  private getItemsAndProductsOfShoppingCart(): Observable<ShoppingCartItemProduct[]> {
-    return this.shoppingCart
-      .pipe(map(cart => cart._embedded.shoppingCartItems))
-      .pipe(
-        flatMap(
-          (shoppingCartItems) => {
-            return forkJoin(shoppingCartItems
-              .map(
-                (shoppingCartItem) => {
-                  return this.productService.getProductFromUrl(shoppingCartItem._links.product.href)
-                    .pipe(map(
-                      (product) => {
-                        const shoppingCartItemProduct = new ShoppingCartItemProduct();
-                        shoppingCartItemProduct.shoppingCartItem = shoppingCartItem;
-                        shoppingCartItemProduct.product = product;
-                        return shoppingCartItemProduct;
-                      }
-                    ));
-                }
-              )
-            );
+  public updateItem(item: ShoppingCartItem): void {
+    this.items$.pipe(
+      take(1),
+      switchMap(items => this.shoppingCartItemService.updateItem(item).pipe(
+        tap(() => items[items.indexOf(item)] = item),
+        map(() => items)
+      ))
+    ).subscribe(items => this.items$.next(items));
+  }
+
+  private getCartItems() {
+    return this.userService.user
+      .pipe(mergeMap(
+        (user) => {
+          if (user) {
+            return this.getItemsAndProducts(user);
+          } else {
+            return of<ShoppingCartItem[]>([]);
           }
-        )
+        }
+      ))
+      .subscribe(items => this.items$.next(items));
+  }
+
+  private getItemsAndProducts(user: User): Observable<ShoppingCartItem[]> {
+    return this.getItemsOfUser(user)
+      .pipe(
+        mergeMap(
+          (items) => {
+            return this.getProductsOfCartItems(items)
+              .pipe(map(products => products.map(
+                (product, i) => {
+                  return new ShoppingCartItem(items[i].quantity, items[i]._links, product);
+                }
+              )));
+          }
+        ),
       );
   }
 
-  private calculateTotal(): Observable<number> {
-    return this.itemsAndProducts
-      .pipe(
-        map((shoppingCartItemsAndProducts) => {
-            return shoppingCartItemsAndProducts
-              .map(shoppingCartItemAndProduct =>
-                shoppingCartItemAndProduct.shoppingCartItem.quantity * shoppingCartItemAndProduct.product.price)
-              .reduce(
-                (a, b) => {
-                  return a + b;
-                }
-              );
-          }
-        )
-      );
+  private getItemsOfUser(user: User): Observable<ShoppingCartItemDTO[]> {
+    return this.http
+      .get<ShoppingCartDTO>(user._links.shoppingCart.href)
+      .pipe(map(cart => cart._embedded.shoppingCartItems));
   }
 
-  private calculateNumberOfProducts(): Observable<number> {
-    return this.itemsAndProducts
-      .pipe(
-        map((shoppingCartItemsAndProducts) => {
-            return shoppingCartItemsAndProducts
-              .map(shoppingCartItemAndProduct => shoppingCartItemAndProduct.shoppingCartItem.quantity)
-              .reduce(
-                (a, b) => {
-                  return a + b;
-                }
-              );
-          }
-        )
-      );
+  private getProductsOfCartItems(cartItems: ShoppingCartItemDTO[]): Observable<Product[]> {
+    return forkJoin(cartItems
+      .map(item => this.productService.getProductFromUrl(item._links.product.href))
+    );
+  }
+
+  private calculateTotal() {
+    this.items$.pipe(
+      map(items => items.length === 0 ? 0 : items.map(item => item.quantity * item.product.price).reduce((a, b) => a + b))
+    ).subscribe(total => this.total$.next(total));
+  }
+
+  private calculateCount() {
+    this.items$.pipe(
+      map(items => items.length === 0 ? 0 : items.map(item => item.quantity).reduce((a, b) => a + b))
+    ).subscribe(total => this.count$.next(total));
   }
 }
