799ab597f65ea2eacfae437bc70b7122a17ef462
Katona Áron <katonaaron01@gmail.com>
2020-01-07 04:08:35 +0200

    product-list changed to update on category change

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {Product} from '../product.model';
 import {ProductService} from '../product.service';
 import {ProductCategory} from '../../categories/product-category.model';
@@ -8,14 +8,14 @@ import {ProductCategory} from '../../categories/product-category.model';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnChanges {
   @Input() productCategory: ProductCategory = null;
   products: Product[] = [];
 
   constructor(private productService: ProductService) {
   }
 
-  ngOnInit() {
+  ngOnChanges() {
     this.productService.getAllProductsFromUrl(this.productCategory._links.products.href)
       .subscribe((productCollection) => {
         this.products = productCollection._embedded.products;
